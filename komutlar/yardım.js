9efb0fecc5c3f120442c38b533acaf57d327c66c
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-31 11:39:04 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -4,14 +4,14 @@ module.exports.run = async (bot, message, args) => {
   let embed = new Discord.RichEmbed()
     .setTitle("KATEGORİLER")
     .setAuthor("85 Tane Komut Var", bot.user.displayAvatarURL)
-    .addField("Moderasyon", "`" + message.prefix + "moderasyon`", true)
+    .addField("Moderasyon", "`" + message.prefix + "mod`", true)
     .addField("Oyunlar", "`" + message.prefix + "oyunlar`", true)
     .addField("Bayraklar", "`" + message.prefix + "bayrak`", true)
     .addField("Müzik", "`" + message.prefix + "Müzik`", true)
     .addField("Eğlence", "`" + message.prefix + "eğlence`", true)
     .addField("İstatistikler", "`" + message.prefix + "istatistikler`", true)
     .addField("Admin", "`" + message.prefix + "admin`", true)
-  .setFooter("Gördüğünüz tüm komutlar JavaScript'tir -" + message.prefix + "yardım" + "- Botun Sahibi: Enes Onur Ata#9427")
+  .setFooter("Gördüğünüz tüm komutlar JavaScript'tir »  " + message.prefix + "yardım" + " »  Sahibi: Enes Onur Ata#9427")
     message.channel.send(embed);
 }
 module.exports.help = {
